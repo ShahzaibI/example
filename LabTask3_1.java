b669af772a8e552da0fc476fd6811a4e589dfe15
Shahzaib Islam <sg03056924122@gmail.com>
2022-03-04 19:41:16 +0500

    Add my new file

---
+++ LabTask3_1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Shahzaib Islam
+ */
+public class LabTask3_1 
+{
+    public static void main(String[] args)
+    {
+        String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        System.out.println("The length of the string is:"+txt.length());
+        String txt1="Hello Word";
+        System.out.println("Upper case= "+txt1.toUpperCase());
+        System.out.println("Lower case= "+txt1.toLowerCase());
+        String txt2="Please locate where 'locate' occurs!";
+        System.out.println(txt2.indexOf("locate"));
+    }    
+}
