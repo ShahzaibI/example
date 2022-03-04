4acb31fdb340d71bd88d32d9a5a122fc197b558f
Shahzaib Islam <sg03056924122@gmail.com>
2022-03-04 19:07:35 +0500

    another file uploaded

---
+++ LabTask2_2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab.task.pkg2_1;
+
+/**
+ *
+ * @author Shahzaib Islam
+ */
+public class LabTask2_2 
+{
+    public static void main(String[] args)
+    {
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println(i);
+        }
+    }
+    
+}
